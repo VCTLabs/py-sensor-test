3875e21d6ce34873f7eb5475e52718bf651a5274
Steve Arnold <stephen.arnold42@gmail.com>
2014-12-23 22:28:43 -0800

    added data logging, initial settings query display

--- python/AS3935-monitor.py
+++ python/AS3935-monitor.py
@@ -32,13 +32,23 @@ sensor = RPi_AS3935(address=0x03, bus=1)
 ## Uncomment to reset all registers to factory default values.
 sensor.reset()
 
-sensor.calibrate(tun_cap=0x03)
+sensor.calibrate(tun_cap=0x05)
 time.sleep(0.002)
 sensor.set_indoors(True)
 sensor.set_noise_floor(0)
 
 ## uncomment/set to filter out false positives
-sensor.set_min_strikes(5)
+sensor.set_min_strikes(1)
+
+def log_raw_data(data):
+    # need to pass time as parameter and convert to UTC here
+    utcnow = datetime.utcnow()
+    version = 01
+    timestamp = utcnow.strftime("%Y-%m-%d %H:%M:%S.%f")
+    outstring = str(version)+" "+str(timestamp)+" "+str(data)+" "+str(min_strikes)+" "+str(noise_floor)+"\n"
+    print(outstring)
+    f.write(outstring)
+    f.flush
 
 def handle_interrupt(channel):
     time.sleep(0.003)
@@ -53,6 +63,8 @@ def handle_interrupt(channel):
     elif reason == 0x08:
         now = datetime.now()
         distance = sensor.get_distance()
+        if 0 < distance < 63:
+            log_raw_data(distance)
         if distance == 1:
             print("Overhead lightning detected - distance = " + str(distance) + " km at %s ") % now.strftime("%H:%M:%S.%f")[:-3],now.strftime("%Y-%m-%d")
         elif 40 < distance < 63:
@@ -68,19 +80,33 @@ cs_pin = 24
 GPIO.setup(irq_pin, GPIO.IN)
 GPIO.add_event_detect(irq_pin, GPIO.RISING, callback=handle_interrupt)
 
+def read_settings():
+    global min_strikes, noise_floor
+    min_strikes = sensor.get_min_strikes()
+    noise_floor = sensor.get_noise_floor()
+
+def print_settings():
+    print("Minimum allowed strikes is " + str(min_strikes))
+    print("Current noise floor is " + str(noise_floor))
+
 running = True
 
 try:
     print("AS3935 Lightning Detection Monitor Script - v0.1")
     print("  Monitor Status: ONLINE")
     print("")
+    ## log raw data to file
+    f=open('AS3935-data.txt','a')
+    read_settings()
 
     while running:
         time.sleep(1.0)
 
 except KeyboardInterrupt:
     print("  Monitor Status: OFFLINE")
+    print_settings()
     print("")
 
 finally:
     GPIO.cleanup() # clean up GPIO on CTRL+C exit
+    f.close()
